79ef138350e0260c84a549502ec55845893cea66
maria <maria.hortalg@gmail.com>
2016-11-06 13:16:55 +0100

    prueba

---
+++ hello.py
@@ -0,0 +1,17 @@
+
+	#############################################################################################
+
+		# Instrucciones de ejecucion: 
+		# - Navegar hasta la ubicacion del archivo
+		# - Teclear 'python hello.py'
+		# - La ejecucion del programa aparece debajo del simbolo del sistema
+
+	#############################################################################################
+
+
+# Nombre: hello.py 
+# Author: Maria Hortal
+# Descripcion: prueba de python
+
+print("Hello, World")
+
